c3979df5d50d8a5fe287f6c82ee36e2d53972616
tiago-silver <tiagopereirasilva5425@gmail.com>
2024-03-15 09:52:21 -0300

    Código final

---
+++ scripts.js
@@ -0,0 +1,68 @@
+alert("Hello, World!")
+//Exercício 1
+// let numberOne = 5
+// let numberTwo = 6
+// let sum = numberOne + numberTwo
+// alert(sum)
+
+//Exercício 3
+// let number = "5"
+
+// if(typeof number === "number"){
+//     alert("É um número")
+// }else{
+//     alert('Não é um número.')
+// }
+
+// Exercício 4
+// let string = "Hello"
+// if(typeof string === "string"){
+//     alert("É umA String")
+// }else{
+//     alert("Não é uma String")
+// }
+
+// Exercício 5
+// let boolean = false
+
+// if(typeof boolean === "boolean"){
+//     alert("É um boolean")
+// }else{
+//     alert("Não é uma boolean")
+// }
+
+// Exercício 6
+// let numberOne = 12
+// let numberTwo = 5
+// let sub = numberOne - numberTwo
+// alert(sub)
+
+// Exercício 7
+// let numberOne = 12
+// let numberTwo = 5
+// let mult = numberOne * numberTwo
+// alert(mult)
+
+// Exercício 8
+// let numberOne = 12
+// let numberTwo = 5
+// let div = numberOne / numberTwo
+// alert(div)
+
+// Exercício 9
+//let number = 12
+
+// if(number % 2 == 0){
+//     alert("É um número PAR")
+// }else{
+//     alert("Não é um número PAR")
+// }
+
+// Exercício 10
+
+let number = 15
+if(number % 2 != 0){
+    alert("É um número ÍMPAR")
+}else{
+    alert("Não é um número ÍMPAR")
+}
